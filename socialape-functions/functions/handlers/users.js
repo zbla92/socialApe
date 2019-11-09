f6e87453d403f5110db12ad6055d856094e699b0
zbla92 <milanblaz2580456@gmail.com>
2019-11-09 19:28:20 +0100

    function Upload image - Completed /n function reduceuserDetails - in progress

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -1,23 +1,25 @@
-const {db} = require('../util/admin');
+const {db, admin} = require('../util/admin');
 const config = require('../util/config');
 
 const firebase = require('firebase');
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData } = require('../util/validators')
+const { validateSignupData, validateLoginData, reduceUserDetails } = require('../util/validators')
 
 exports.signup = (req, res) => {
     const newUser = {
         email: req.body.email,
         password: req.body.password,
         confirmPassword: req.body.confirmPassword,
-        handle: req.body.handle,
+        handle: req.body.handle
     };
 
 
     const { valid, errors } = validateSignupData(newUser);
 
     if(!valid) return res.status(400).json(errors);
+
+    const noImg = 'no-img.png';
 
     let token,userId;
     db.doc(`/users/${newUser.handle}`)
@@ -41,6 +43,7 @@ exports.signup = (req, res) => {
                 handle: newUser.handle,
                 email: newUser.email,
                 createdAt: new Date().toISOString(),
+                imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
                 userId
             };
             return db.doc(`/users/${newUser.handle}`).set(userCredentials)
@@ -86,15 +89,72 @@ exports.login = (req, res) => {
 
 }
 
-exports.uploadImage = (req, res) => {
-    const busboy = require('busboy');
-    const path = require('path');
-    const os = require('os');
-    const fs = require('fs');
+// Add user details
+exports.addUserDetails = (req, res) => {
+  let userDetails = reduceUserDetails(req.body);
 
-    const busboy = new busboy({ headers: res.headers });
-
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-        const imageExtension = filename.split('.')[filename.split('.').length - 1]
+  db.doc(`/users/${req.user.handle}`).update(userDetails)
+    .then(() => {
+      return res.json({ message: 'Details added successfully' });
+    })
+    .catch(err => {
+      return res.status(500).json({error: err.code})
     })
 }
+
+
+// Upload a profile image for user
+exports.uploadImage = (req, res) => {
+  const BusBoy = require('busboy');
+  const path = require('path');
+  const os = require('os');
+  const fs = require('fs');
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageToBeUploaded = {};
+  let imageFileName;
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
+    }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on('finish', () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          config.storageBucket
+        }/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: 'image uploaded successfully' });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: 'something went wrong' });
+      });
+  });
+  busboy.end(req.rawBody);
+};
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth')
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails } = require('./handlers/users');
 
 // Scream routs
 app.get('/screams', getAllScreams);
@@ -14,7 +14,8 @@ app.post('/scream', FBAuth, postOneScream);
 // Users route
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('./user/image', uploadImage);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails)
 
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
