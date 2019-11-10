9d4e3199c379505606b8b143f0beffcdf8c19adf
zbla92 <milanblaz2580456@gmail.com>
2019-11-10 15:11:48 +0100

    built function for commenting and getting comments

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -102,6 +102,30 @@ exports.addUserDetails = (req, res) => {
     })
 }
 
+// Get own user Details
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.user.handle}`).get()
+    .then(doc => {
+      if(doc.exists){
+        userData.credentials = doc.data();
+        return db.collection('likes').where('userHandle', '==', req.user.handle).get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      })
+      return res.json(userData)
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({error: err.code})
+    })
+
+} 
+
 
 // Upload a profile image for user
 exports.uploadImage = (req, res) => {
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -4,18 +4,25 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth')
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails } = require('./handlers/users');
+const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
 // Scream routs
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+// ToDo: delete scream
+// ToDo: Like scream
+// ToDo: Unlike scream
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
+
 
 // Users route
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails)
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
