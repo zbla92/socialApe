f39ca8f4e78d3e42913049c42256a2e0f55334fd
zbla92 <milanblaz2580456@gmail.com>
2019-11-11 15:34:54 +0100

    notification functions push

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions'); 
-
 const app = require('express')();
+const FBAuth = require('./util/fbAuth');
 
-const FBAuth = require('./util/fbAuth')
+const { db } = require('./util/admin')
 
 const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
@@ -26,3 +26,57 @@ app.get('/user', FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions.region('europe-west1').firestore.document(`likes/{id}`)
+    .onCreate(snapshot => {
+        db.doc(`scream/${snapshot.data().screamId}`).get()
+            .then( doc => {
+                if(doc.exists){
+                    return db.doc(`notifications/${snapshot.id}`).set({
+                        createdAt: new Date().toISOString(),
+                        recipient: doc.data().userHandler,
+                        sender: snapshoit.data().userHandler,
+                        type: 'like',
+                        read: false,
+                        screamId: doc.id
+                    })
+                }
+            })
+            .then(() => {
+                return;
+            })
+            .catch(err => {
+                console.error(err);
+                return 
+            })
+    })
+exports.deleteNotificationOnUnlike = functions.region('europe-west1').firestore.document('likes/{id}')
+    .onDelete(snapshot => {
+        db.doc(`/notifications/${snapshot.id}`)
+            .delete()
+            .then(() => {
+                return
+            })
+            .catch(err => {
+                console.error(err);
+                return
+            })
+    })
+
+exports.createNotificationOnComment = functions.region('europe-west1').firestore.document(`comments/{id}`)
+    .onCreate(snapshot => {
+        db.doc(`/screams/${snapshot.data().screamId}`).get()
+        .then(doc => {
+            if(doc.exists){
+                return db.doc(`/notifications/${snapshot.if}`).set({
+                    createdAt: new Date().toISOString(),
+                    recipient: doc.data().userHandler,
+                    sender: snapshoit.data().userHandler,
+                    type: 'comment',
+                    read: false,
+                    screamId: doc.id
+                })
+            }
+        })
+    })
+
