0c3019ce2f254d3363789e36031ac11776faca7b
zbla92 <milanblaz2580456@gmail.com>
2019-11-12 21:12:26 +0100

    backend complete

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -56,7 +56,7 @@ exports.signup = (req, res) => {
             if(err.code === 'auth/email-already-in-use'){
                 return res.status(400).json({email: 'Email is already in use'})
             } else {
-                return res.status(500).json({ error: err })
+                return res.status(500).json({ general: "Something went wrong, please try again" })
             }
             
         })
@@ -82,9 +82,7 @@ exports.login = (req, res) => {
         })
         .catch(err => {
             console.error(err);
-            if(err.code === 'auth/wrong-password'){
                 return res.status(403).json({general: "Wrong Credentials, please try again"})
-            }else return res.status(500).json({error: err.code})
         })
 
 }
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -38,10 +38,7 @@ exports.createNotificationOnLike = functions
       .doc(`/screams/${snapshot.data().screamId}`)
       .get()
       .then((doc) => {
-        if (
-          doc.exists &&
-          doc.data().userHandle !== snapshot.data().userHandle
-        ) {
+        if ( doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
@@ -74,10 +71,7 @@ exports.createNotificationOnComment = functions
       .doc(`/screams/${snapshot.data().screamId}`)
       .get()
       .then((doc) => {
-        if (
-          doc.exists &&
-          doc.data().userHandle !== snapshot.data().userHandle
-        ) {
+        if (doc.exists && doc.data().userHandle !== snapshot.data().userHandle) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
@@ -93,3 +87,49 @@ exports.createNotificationOnComment = functions
         return;
       });
   });
+
+  exports.onUserImageChange = functions.region('europe-west1').firestore.document('/users/{userId}')
+    .onUpdate(change => {
+        console.log(change.before.data())
+        console.log(change.after.data())
+        if(change.before.data().imageUrl !== change.after.data().imageUrl){
+            console.log('img has changed')
+            const  batch = db.batch();
+            return db.collection('screams').where('userHandle', '==', change.before.data().handle).get()
+                .then(data => {
+                    data.forEach(doc => {
+                        const scream = db.doc(`/screams/${doc.id}`);
+                        batch.update(scream, {userImage: change.after.data().imageUrl})
+                    })  
+                    return batch.commit();
+                })
+        }
+    })
+
+    exports.onScreamDelete = functions.region('europe-west1').firestore.document('screams/{screamId}')
+        .onDelete((snapshot, context) => {
+            const screamId = context.params.screamId;
+            const batch = db.batch();
+            return db.collection('comments').where('screamId', '==', screamId).get()
+                .then(data => {
+                    data.forEach(doc => {
+                        batch.delete(db.doc(`/comments/${doc.id}`))
+                    })
+                    return db.collection('likes').where('screamId', '==', screamId).get();
+                })
+                .then(data => {
+                    data.forEach(doc => {
+                        batch.delete(db.doc(`/likes/${doc.id}`))
+                    })
+                    return db.collection('notifications').where('screamId', '==', screamId).get();
+                })
+                .then(data => {
+                    data.forEach(doc => {
+                        batch.delete(db.doc(`/notifications/${doc.id}`))
+                    })
+                    return batch.commit();
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+        })
