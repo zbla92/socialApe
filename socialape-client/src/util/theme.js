d733e1a4b537bae58c2fe0e3d1f30d1ba385bb59
zbla92 <milanblaz2580456@gmail.com>
2019-11-14 14:07:40 +0100

    preRedux implementation stage

---
+++ socialape-client/src/util/theme.js
@@ -0,0 +1,41 @@
+export const globalTheme = {
+    palette: {
+      primary: {
+        light: '#9bc0ff',
+        main: '#82b1ff',
+        dark: '#5b7bb2',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: "#ffb199",
+        main: '#ff9e80',
+        dark: '#b26e59',
+        contrastText: '#fff'
+      }
+    },
+    spreadThis: {
+      form:{
+        textAlign: 'center',
+      },
+      image: {
+          margin: '20px auto 20px auto'
+      },
+      pageTitle: {
+          margin: '10px auto 10px auto'
+      },
+      textField: {
+          margin: '10px auto 10px auto'
+      },
+      button:{
+          marginTop: '20px',
+          position: "relative"
+      },
+      customError:{
+          color: 'red',
+          fontSize: '0.8rem'
+      },
+      progress:{
+          position: 'absolute',
+      }
+    }
+}
