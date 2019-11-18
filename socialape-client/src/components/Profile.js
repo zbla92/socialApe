50caaba90bff06839b913a835b184496eb94581e
zbla92 <milanblaz2580456@gmail.com>
2019-11-18 01:24:15 +0100

    logout and edit profile - working '// navbar buttons after logging in working

--- socialape-client/src/components/Profile.js
+++ socialape-client/src/components/Profile.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import EditDetails from './EditDetails';
+import MyButton from '../util/MyButton';
 
 // MUI stuff
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip' 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
 import LinkIcon from '@material-ui/icons/Link';
@@ -102,11 +102,9 @@ class Profile extends Component{
                     <div className="image-wrapper">
                         <img src={imageUrl} alt='profile' className='profile-image' />
                         <input type="file" id="imageInput" hidden='hidden' onChange={this.handleImageChange}/>
-                        <Tooltip title="Edit profile picture" placement='top'>
-                            <IconButton onClick={this.handleEditPicture} className='button'>
-                                <EditIcon color="primary"/>
-                            </IconButton>
-                        </Tooltip>
+                        <MyButton tip='Edit profile picture' onClick={this.handleEditPicture} btnClassname='button'>
+                            <EditIcon color='primary' />
+                        </MyButton>
                     </div>
                     <hr/>
                     <div className="profile-details">
@@ -134,13 +132,10 @@ class Profile extends Component{
                         <CalendarToday color="primary"/> {' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
-                    <Tooltip title="Logout" placement="top">
-                        <IconButton onClick={this.handleLogout}>
-                            <KeyboardReturn color="primary">
-
-                            </KeyboardReturn>
-                        </IconButton>
-                    </Tooltip>
+                    <MyButton tip='Logout' onClick={this.handleLogout} btnClassname='button'>
+                        <KeyboardReturn color="primary"/>
+                    </MyButton>
+                    <EditDetails />
                 </div>
             </Paper>
         ) : (
--- socialape-client/src/redux/actions/userActions.js
+++ socialape-client/src/redux/actions/userActions.js
@@ -66,6 +66,16 @@ export const uploadImage = formData => dispatch => {
         })
 }
 
+export const editUserDetails = userDetails => dispatch => {
+    dispatch({type: LOADING_USER});
+    axios.post('/user', userDetails)
+        .then(() => {
+            dispatch(getUserData())
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
 
 const setAuthorizationHeader = token => {
     const FBIdToken = `Bearer ${token}`
